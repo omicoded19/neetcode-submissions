d36883d8228b622c094ec19f95342bc8925f1d7b
Omdeep <omicoded19@gmail.com>
2026-07-01 01:50:12 +0530

    Add: next-greater-element-i - submission-1

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>st;
+        unordered_map<int,int>mpp;
+        for(int num:nums2){
+            while(!st.empty() && num>st.top()){
+                mpp[st.top()]=num;
+                st.pop();
+            }
+
+            st.push(num);
+        }
+        while(!st.empty()){
+            mpp[st.top()]=-1;
+            st.pop();
+        }
+        vector<int>ans;
+        for(int num:nums1){
+            ans.push_back(mpp[num]);
+        }
+        return ans;
+    }
+
+};
