65a62e25386116bd4d834a985e6435d6a92ca136
Omdeep <omicoded19@gmail.com>
2026-07-04 16:56:42 +0530

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution{
+public:
+    bool isIsomorphic(string s,string t){
+        unordered_map<char,char>m1;
+        unordered_map<char,char>m2;
+
+        for(int i=0;i<s.size();i++){
+            if(m1.count(s[i])&&m1[s[i]]!=t[i])return false;
+            if(m2.count(t[i])&&m2[t[i]]!=s[i])return false;
+
+            m1[s[i]]=t[i];
+            m2[t[i]]=s[i];
+        }
+
+        return true;
+    }
+};
